4472e39b619445783864b2ee6d5b8b3ece507ecb
Riley Meyerkorth <rileymeyerkorth@gmail.com>
2024-01-15 11:19:55 -0600

    add bootstrap

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,28 @@
-import './App.css'
-import './ProjectCard'
-import ProjectCard from './ProjectCard';
+import './components/ProjectCard'
+import ProjectGrid from './components/ProjectGrid';
+import Project from './Project';
+import Navigation from "./components/Navigation";
+
+// Test data and projects
+const testproject = new Project("Project 1", "This is a test project!", "https://www.google.com/url?sa=i&url=https%3A%2F%2Funsplash.com%2Fs%2Fphotos%2Fhuman&psig=AOvVaw2IwAZ477AIlNpviSfh4nwC&ust=1705374546489000&source=images&cd=vfe&opi=89978449&ved=0CBMQjRxqFwoTCKCKoqW13oMDFQAAAAAdAAAAABAQ");
+const testproject2 = new Project("Project 2", "This is a test project!", "https://www.google.com/url?sa=i&url=https%3A%2F%2Funsplash.com%2Fs%2Fphotos%2Fhuman&psig=AOvVaw2IwAZ477AIlNpviSfh4nwC&ust=1705374546489000&source=images&cd=vfe&opi=89978449&ved=0CBMQjRxqFwoTCKCKoqW13oMDFQAAAAAdAAAAABAQ");
+const testproject3 = new Project("Project 3", "This is a test project!", "https://www.google.com/url?sa=i&url=https%3A%2F%2Funsplash.com%2Fs%2Fphotos%2Fhuman&psig=AOvVaw2IwAZ477AIlNpviSfh4nwC&ust=1705374546489000&source=images&cd=vfe&opi=89978449&ved=0CBMQjRxqFwoTCKCKoqW13oMDFQAAAAAdAAAAABAQ");
+const testproject4 = new Project("Project 4", "This is a test project!", "https://www.google.com/url?sa=i&url=https%3A%2F%2Funsplash.com%2Fs%2Fphotos%2Fhuman&psig=AOvVaw2IwAZ477AIlNpviSfh4nwC&ust=1705374546489000&source=images&cd=vfe&opi=89978449&ved=0CBMQjRxqFwoTCKCKoqW13oMDFQAAAAAdAAAAABAQ");
+const testproject5 = new Project("Project 5", "This is a test project!", "https://www.google.com/url?sa=i&url=https%3A%2F%2Funsplash.com%2Fs%2Fphotos%2Fhuman&psig=AOvVaw2IwAZ477AIlNpviSfh4nwC&ust=1705374546489000&source=images&cd=vfe&opi=89978449&ved=0CBMQjRxqFwoTCKCKoqW13oMDFQAAAAAdAAAAABAQ");
+const testproject6 = new Project("Project 6", "This is a test project!", "https://www.google.com/url?sa=i&url=https%3A%2F%2Funsplash.com%2Fs%2Fphotos%2Fhuman&psig=AOvVaw2IwAZ477AIlNpviSfh4nwC&ust=1705374546489000&source=images&cd=vfe&opi=89978449&ved=0CBMQjRxqFwoTCKCKoqW13oMDFQAAAAAdAAAAABAQ");
+const testproject7 = new Project("Project 7", "This is a test project!", "https://www.google.com/url?sa=i&url=https%3A%2F%2Funsplash.com%2Fs%2Fphotos%2Fhuman&psig=AOvVaw2IwAZ477AIlNpviSfh4nwC&ust=1705374546489000&source=images&cd=vfe&opi=89978449&ved=0CBMQjRxqFwoTCKCKoqW13oMDFQAAAAAdAAAAABAQ");
+const testproject8 = new Project("Project 8", "This is a test project!", "https://www.google.com/url?sa=i&url=https%3A%2F%2Funsplash.com%2Fs%2Fphotos%2Fhuman&psig=AOvVaw2IwAZ477AIlNpviSfh4nwC&ust=1705374546489000&source=images&cd=vfe&opi=89978449&ved=0CBMQjRxqFwoTCKCKoqW13oMDFQAAAAAdAAAAABAQ");
+const testproject9 = new Project("Project 9", "This is a test project!", "https://www.google.com/url?sa=i&url=https%3A%2F%2Funsplash.com%2Fs%2Fphotos%2Fhuman&psig=AOvVaw2IwAZ477AIlNpviSfh4nwC&ust=1705374546489000&source=images&cd=vfe&opi=89978449&ved=0CBMQjRxqFwoTCKCKoqW13oMDFQAAAAAdAAAAABAQ");
+
+const projects = [testproject, testproject2, testproject3, testproject4, testproject5, testproject6, testproject7, testproject8, testproject9];
 
 function App() {
   return (
-    <ProjectCard />
+    <div className="bg-primary">
+      <Navigation></Navigation>
+      <h1 className="">Projects</h1>
+      <ProjectGrid projects={projects}/>
+    </div>
   );
 }
 
