8d3af6798e1611c29ecd24ef5e0ee67bab42fefa
Riley Meyerkorth <rileymeyerkorth@gmail.com>
2024-01-15 21:33:44 -0600

    add methods for getting release urls

--- src/classes/Project.ts
+++ src/classes/Project.ts
@@ -1,6 +1,7 @@
 class Project {
     name:string;
     description:string;
+    author:string;
     dateCreated:string;
     dateUpdated:string;
     datePushed:string;
@@ -8,11 +9,11 @@ class Project {
     topics:Array<string>;
     url:string;
     image:string;
-    
 
     constructor(
         name:string="", 
         description:string="", 
+        author:string="",
         dateCreated:string="",
         dateUpdated:string="",
         datePushed:string="",
@@ -22,15 +23,27 @@ class Project {
         image:string=""
         ) {
 
-        this.name = name;
-        this.description = description;
-        this.dateCreated = dateCreated;
-        this.dateUpdated = dateUpdated;
-        this.datePushed = datePushed;
-        this.language = language;
-        this.topics = topics;
-        this.url = url;
-        this.image = image;
+        this.name           = name;
+        this.description    = description;
+        this.author         = author;
+        this.dateCreated    = dateCreated;
+        this.dateUpdated    = dateUpdated;
+        this.datePushed     = datePushed;
+        this.language       = language;
+        this.topics         = topics;
+        this.url            = url;
+        this.image          = image;
+    }
+
+    public getReleasesUrl() : string {
+        return "https://api.github.com/repos/" + this.author + "/" + this.name + "/releases"
+    }
+
+    /*
+    Returns the latest release URL of the project.
+    */
+    public getLatestReleaseUrl() : string {
+        return "https://api.github.com/repos/" + this.author + "/" + this.name + "/releases/latest"
     }
 }
 
