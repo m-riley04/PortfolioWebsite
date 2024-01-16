660f3ffffa1c2b409443911e7530a06fc2853b87
Riley Meyerkorth <rileymeyerkorth@gmail.com>
2024-01-16 14:15:13 -0600

    add link group

--- src/components/NavigationBar.tsx
+++ src/components/NavigationBar.tsx
@@ -7,13 +7,17 @@ function NavigationBar() {
   return (
     <Navbar expand="lg" className="navbar">
       <Container>
-        <Navbar.Brand href="#home">Portfolio</Navbar.Brand>
+        <Navbar.Brand href="#home">Riley Meyerkorth</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
-            <Nav.Link href="#home" className="navbar-option">Projects</Nav.Link>
-            <Nav.Link href="https://github.com/m-riley04" target="_blank" className="navbar-option">GitHub</Nav.Link>
-            <Nav.Link href="https://www.linkedin.com/in/riley-meyerkorth/" target="_blank" className="navbar-option">LinkedIn</Nav.Link>
+            <Nav.Link href="#repositories" className="navbar-option">Repositories</Nav.Link>
+            <Nav.Link href="#projects" className="navbar-option">Projects</Nav.Link>
+            <NavDropdown title="Links" id="basic-nav-dropdown" className="navbar-option navbar-dropdown">
+              <NavDropdown.Item href="https://github.com/m-riley04" target="_blank">GitHub</NavDropdown.Item>
+              <NavDropdown.Item href="https://www.linkedin.com/in/riley-meyerkorth" target="_blank">LinkedIn</NavDropdown.Item>
+              <NavDropdown.Item href="https://restlessmedicine.com" target="_blank">Company</NavDropdown.Item>
+            </NavDropdown>
             <NavDropdown title="Debug" id="basic-nav-dropdown" className="navbar-option navbar-dropdown">
               <NavDropdown.Item href="#action/add">Add New Project</NavDropdown.Item>
               <NavDropdown.Item href="#action/remove">Remove Project</NavDropdown.Item>
