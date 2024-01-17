1f42a4f9001485214e0aa2c4d39467102999dc96
Riley Meyerkorth <rileymeyerkorth@gmail.com>
2024-01-17 16:12:30 -0600

    changed some cluttering error messages

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -2,9 +2,10 @@ module.exports = {
   root: true,
   env: { browser: true, es2020: true },
   extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:react-hooks/recommended',
+    //'eslint:recommended',
+    //'plugin:@typescript-eslint/recommended',
+    //'plugin:react-hooks/recommended',
+    'plugin:react/recommended',
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parser: '@typescript-eslint/parser',
@@ -14,5 +15,8 @@ module.exports = {
       'warn',
       { allowConstantExport: true },
     ],
+    'react/react-in-jsx-scope' : [
+      'off'
+    ]
   },
 }
