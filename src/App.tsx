7cfcd4a99d20cfbaa1de1b37993f036f1e75fb67
Riley Meyerkorth <rileymeyerkorth@gmail.com>
2024-01-17 12:05:21 -0600

    create base page system with key-value map

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,39 @@
+import HomePage from "./components/pages/HomePage";
 import NavigationBar from "./components/NavigationBar";
-import RepositoryPage from './components/RepositoryPage'
-//import {useState} from 'react';
+import RepositoryPage from './components/pages/RepositoryPage'
+import {useState} from 'react';
 
 function App() {
   //#region STATES
   // Pages
+  const [page, setPage] = useState("#repositories");
 
   /**
-   * The current page that the website is on.
-   * @type {number} An integer representing the current page/component:
-   * - 0: Home
-   * - 1: Repositories
-   * - 2: Projects
+   * The map of availaible pages 
+   * @type {string : JSX.Element} A map of 
+   * - #home
+   * - #repositories
+   * - #projects
    */
-  //const [page, setPage] = useState(0);
+  const pages : {[name : string] : JSX.Element} = {
+    "#home": <HomePage/>,
+    "#repositories": <RepositoryPage/>,
+    "#projects": <></>,
+    "#tools": <></>,
+    "#fun": <></>
+  };
+
+  const handleNavigation = (e:MouseEvent, pageName:string) => { 
+    // Set the current page name
+    setPage(pageName);
+  }
   //#endregion
 
   return (
     <div className="bg-primary">
       <NavigationBar />
       <div className="main-container">
-        <RepositoryPage/>
+        {pages[page]}
       </div>
     </div>
   );
--- src/components/NavigationBar.tsx
+++ src/components/NavigationBar.tsx
@@ -3,7 +3,7 @@ import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
 
-function NavigationBar() {
+function NavigationBar({ onButtonClick } : {onButtonClick?:(e:MouseEvent, buttonName:string) => void}) {
   return (
     <Navbar expand="lg" className="navbar">
       <Container>
@@ -11,19 +11,18 @@ function NavigationBar() {
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
-            <Nav.Link href="#repositories" className="navbar-option">Repositories</Nav.Link>
-            <Nav.Link href="#projects" className="navbar-option">Projects</Nav.Link>
+            <Nav.Link id="option-repositories" href="#repositories" className="navbar-option">Repositories</Nav.Link>
+            <Nav.Link id="option-projects" href="#projects" className="navbar-option">Projects</Nav.Link>
             <NavDropdown title="Links" id="basic-nav-dropdown" className="navbar-option navbar-dropdown">
               <NavDropdown.Item href="https://github.com/m-riley04" target="_blank">GitHub</NavDropdown.Item>
               <NavDropdown.Item href="https://www.linkedin.com/in/riley-meyerkorth" target="_blank">LinkedIn</NavDropdown.Item>
               <NavDropdown.Item href="https://restlessmedicine.com" target="_blank">Company</NavDropdown.Item>
             </NavDropdown>
             <NavDropdown title="Debug" id="basic-nav-dropdown" className="navbar-option navbar-dropdown">
-              <NavDropdown.Item href="#action/add">Add New Project</NavDropdown.Item>
-              <NavDropdown.Item href="#action/remove">Remove Project</NavDropdown.Item>
-              <NavDropdown.Item href="#action/clear">Clear Projects</NavDropdown.Item>
+              <NavDropdown.Item href="#action/test">Test</NavDropdown.Item>
               <NavDropdown.Divider />
-              <NavDropdown.Item href="#action/refresh">Refresh Projects</NavDropdown.Item>
+              <NavDropdown.Item href="#action/refresh-repositories">Refresh Repositories</NavDropdown.Item>
+              <NavDropdown.Item href="#action/refresh-projects">Refresh Projects</NavDropdown.Item>
             </NavDropdown>
           </Nav>
         </Navbar.Collapse>
