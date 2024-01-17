a8326bfb45f05a16b13206de2b2b366341b7ea3a
Riley Meyerkorth <rileymeyerkorth@gmail.com>
2024-01-17 17:40:01 -0600

    remove onClick and test nav button

--- src/components/repository/RepositoryGrid.tsx
+++ src/components/repository/RepositoryGrid.tsx
@@ -1,14 +1,13 @@
 import RepositoryGridCard from './RepositoryGridCard'
 import RepositoryData from '../../classes/RepositoryData';
-import NavigationButton_App from '../navigation/NavigationButton_App'
 
-function RepositoryGrid( {repos, onCardClicked} : {repos:Array<RepositoryData>, onCardClicked:(e:MouseEvent, data:RepositoryData) => void} ) {
+function RepositoryGrid( {repos, onCardClicked} : {repos:Array<RepositoryData>, onCardClicked?:(e:MouseEvent) => void} ) {
 
     const html = [];
     for (let i = 0; i < repos.length; i++) {
         // Create the project's card component
         const repo = repos[i];
-        const repoCard = RepositoryGridCard({data: repo, onClick:(e, data) => onCardClicked(e, data)});
+        const repoCard = RepositoryGridCard({data: repo});
 
         // Push it to the html stack
         html.push(repoCard);
@@ -17,7 +16,6 @@ function RepositoryGrid( {repos, onCardClicked} : {repos:Array<RepositoryData>, 
     return (
         <>
             <h1>Repositories</h1>
-            <NavigationButton_App title="Home" target="#home"></NavigationButton_App>
             <div className="grid">
                 {html}
             </div>
