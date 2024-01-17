2f4e5b014afbdfcc6d3f314eea9f109af5240b0a
Riley Meyerkorth <rileymeyerkorth@gmail.com>
2024-01-17 12:07:32 -0600

    add more columns and rows

--- src/components/repository/Repository.tsx
+++ src/components/repository/Repository.tsx
@@ -4,17 +4,27 @@ function Repository({ data } : { data:RepositoryData }) {
 
 
     return (
-        //
         <div className="container repo">
             <div className="row">
                 <div className="col">
-                    <h1>Repository Name</h1>
+                    <h1>{data.name}</h1>
+                </div>
+                <div className="col">
+                    
+                </div>
+                <div className="col">
+
+                </div>
+            </div>
+            <div className="row">
+                <div className="col">
+                    <p>{data.language}</p>
                 </div>
                 <div className="col">
 
                 </div>
                 <div className="col">
-
+                    
                 </div>
             </div>
         </div>
