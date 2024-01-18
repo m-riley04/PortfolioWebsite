ef282140b7c8bbd94f9cfb942411d71a0ad0ba4e
Riley Meyerkorth <rileymeyerkorth@gmail.com>
2024-01-17 20:53:09 -0600

    remove old navigation functionality

--- src/App.tsx
+++ src/App.tsx
@@ -8,38 +8,7 @@ import AppPageContext from './components/contexts/AppPageContext.ts';
 const PAGE_DEFAULT = "#repositories"
 
 function App() {
-  //#region STATES
-  // Pages
-  /**
-     * The map of availaible pages 
-     * @type {string : JSX.Element} A map of the main pages 
-     * - #home
-     * - #repositories
-     * - #projects
-     * - #tools
-     * - #fun
-     */
-  const pages : {[name : string] : JSX.Element} = {
-    "#home": <HomePage/>,
-    "#repositories": <RepositoryPage/>,
-    "#projects": <></>,
-    "#tools": <></>,
-    "#fun": <></>
-  };
-
-  const [page, setPage] = useState(PAGE_DEFAULT);
-  const pageValue = {page, setPage}
-  //#endregion
-
   return (
-    <AppPageContext.Provider value={pageValue}>
-      <div className="bg-primary">
-        <NavigationBar />
-        <div className="main-container">
-          {pages[page]}
-        </div>
-      </div>
-    </AppPageContext.Provider>
   );
 }
 
