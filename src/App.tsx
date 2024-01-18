3674af0de97532a690748ac7e4a5928bc91235cf
Riley Meyerkorth <rileymeyerkorth@gmail.com>
2024-01-18 12:51:30 -0600

    consolidate routes into dedicated component

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,18 @@
 import NavigationBar from "./components/navigation/NavigationBar";
-
-import HomePage from "./components/pages/HomePage";
-import ToolsPage from "./components/pages/ToolsPage.tsx";
-import RepositoryPage from './components/pages/RepositoryPage'
-import NotFoundPage from './components/pages/NotFoundPage'
+import AnimatedRoutes from "./components/AnimatedRoutes";
+import Footer from "./components/Footer";
 import {
   BrowserRouter as Router,
-  Routes,
-  Route,
-  Navigate
 } from "react-router-dom";
 
 function App() {
   return (
-    <Router>
-      <NavigationBar />
-      <Routes>
-        <Route path="/" element={<HomePage/>}></Route>
-        <Route path="/repositories" element={<RepositoryPage/>}></Route>
-        <Route path="/tools" element={<ToolsPage/>}></Route>
-        <Route path="*" element={<NotFoundPage/>}></Route>
-      </Routes>
-    </Router>
+    <div id="app">
+      <Router>
+        <NavigationBar />
+        <AnimatedRoutes/>
+      </Router>
+    </div>
   );
 }
 
---
+++ src/components/AnimatedRoutes.tsx
@@ -0,0 +1,27 @@
+
+import HomePage from "./pages/HomePage";
+import RepositoryPage from "./pages/RepositoryPage";
+import ToolsPage from "./pages/ToolsPage";
+import NotFoundPage from "./pages/NotFoundPage";
+import { 
+    Route, 
+    Routes, 
+    useLocation
+} from "react-router-dom";
+import { AnimatePresence } from "framer-motion";
+
+function AnimatedRoutes() {
+    const location = useLocation();
+    return (
+        <AnimatePresence mode="wait">
+            <Routes location={location} key={location.pathname}>
+                <Route path="/" element={<HomePage/>}/>
+                <Route path="/repositories" element={<RepositoryPage/>}/>
+                <Route path="/tools" element={<ToolsPage/>}/>
+                <Route path="*" element={<NotFoundPage/>}/>
+            </Routes>
+        </AnimatePresence>
+    );
+}
+
+export default AnimatedRoutes;
