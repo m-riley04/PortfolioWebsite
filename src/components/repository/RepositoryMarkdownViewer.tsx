045805d72f8f513e78d393e3125f36c98269f451
Riley Meyerkorth <rileymeyerkorth@gmail.com>
2024-01-19 00:39:49 -0600

    give functionality

--- src/components/repository/RepositoryMarkdownViewer.tsx
+++ src/components/repository/RepositoryMarkdownViewer.tsx
@@ -1,38 +1,48 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import Markdown from "react-markdown";
 
 /**
  * @param {string} src A url to a markdown file 
  */
-function RepositoryMarkdownViewer( { src, repositoryRefreshed } : { src: string, repositoryRefreshed?:boolean } ) {
+function RepositoryMarkdownViewer( { src } : { src: string } ) {
     
+    // Set up states and hooks
     const [text, setText] = useState("");
+    const [loaded, setLoaded] = useState(false);
 
     /**
-     * Fetches the markdown text from the given src url
+     * Fetches the markdown text from src url property
      */
     const fetchSource = () => {
+        console.log("Fetching the markdown source...")
         fetch( src )
             .then ( (response) => {
+                if (!response.ok) {
+                    setText("### There was an issue fetching the repository's README. \nIt might be because 'README.md' does not exist at the repository's root.")
+                    throw new Error("ERROR: There was an error fetching the markdown text.")
+                }
                 return response.text();
             })
             .then( (text) => {
                 setText(text);
             }).catch ( (err) => {
-                console.log(`There was an error fetching the markdown text: ${err}`)
+                console.error(err);
             })
     }
 
-    // Check if the repository has changed
-    if (repositoryRefreshed) {
-        // Fetch the src
-        fetchSource()
+    // Fetch the source when the page is loaded
+    if (!loaded) {
+        fetchSource();
 
-        // Reset the repositoryChanged flag
-        repositoryRefreshed = true;
+        // Set the loaded flag
+        setLoaded(true);
     }
+
     return (
-        <div style={{overflow: "scroll"}}>
-            
+        <div className="markdown-viewer">
+            <Markdown>
+                {text}
+            </Markdown>
         </div>
     );
 }
