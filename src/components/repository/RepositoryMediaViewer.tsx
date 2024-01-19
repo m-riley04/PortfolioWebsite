6b8814a1f20d42687e8b31a798f48bf21b0369b8
Riley Meyerkorth <rileymeyerkorth@gmail.com>
2024-01-19 13:03:41 -0600

    added url/image mapping

--- src/components/repository/RepositoryMediaViewer.tsx
+++ src/components/repository/RepositoryMediaViewer.tsx
@@ -3,7 +3,7 @@ function RepositoryMediaViewer( { urls } : { urls?:Array<string>}) {
 
     return (
         <div className="media-viewer">
-
+            {urls?.map((url, index) => <img src={url} alt={url} key={index}></img>)}
         </div>
     );
 }
