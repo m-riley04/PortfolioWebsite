3fb1f1ca040f8450a45b163aa2643ad50d21d69e
Riley Meyerkorth <rileymeyerkorth@gmail.com>
2024-01-19 11:58:32 -0600

    implement mapping

--- src/components/repository/RepositoryGrid.tsx
+++ src/components/repository/RepositoryGrid.tsx
@@ -1,23 +1,12 @@
 import RepositoryGridCard from './RepositoryGridCard'
 import RepositoryData from '../../classes/RepositoryData';
 
-function RepositoryGrid( {repos, onCardClicked} : {repos:Array<RepositoryData>, onCardClicked?:(e:MouseEvent) => void} ) {
-
-    const html = [];
-    for (let i = 0; i < repos.length; i++) {
-        // Create the project's card component
-        const repo = repos[i];
-        const repoCard = RepositoryGridCard({data: repo});
-
-        // Push it to the html stack
-        html.push(repoCard);
-    }
-
+function RepositoryGrid( {repos} : {repos:Array<RepositoryData>} ) {
     return (
         <>
             <h1>Repositories</h1>
             <div className="grid">
-                {html}
+                {repos.map((repo, index) => <RepositoryGridCard data={repo} key={index}></RepositoryGridCard>)}
             </div>
         </>
     );
