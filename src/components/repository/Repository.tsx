736580c0e5201f382fb86997c253d2d349d3539d
Riley Meyerkorth <rileymeyerkorth@gmail.com>
2024-01-19 00:40:58 -0600

    added skeleton components to the layout

--- src/components/repository/Repository.tsx
+++ src/components/repository/Repository.tsx
@@ -1,11 +1,24 @@
 import RepositoryData from "../../classes/RepositoryData";
+import {
+    Carousel, CarouselItem
+} from "react-bootstrap"
+import RepositoryMarkdownViewer from "./RepositoryMarkdownViewer"
+import { useEffect, useState } from "react";
 
 function Repository({ data } : { data:RepositoryData }) {
+    
+    const [imageUrls, setImageUrls] = useState([]);
+
+    useEffect(() => {
+        setImageUrls(data.getImageUrls("assets"));
+    }, []);
+
     return (
         <div className="container repo">
             <div className="row">
-                <div className="col">
+                <div className="col-6">
                     <h1>{data.name}</h1>
+                    <p>{data.language}</p>
                 </div>
                 <div className="col">
                     
@@ -15,14 +28,18 @@ function Repository({ data } : { data:RepositoryData }) {
                 </div>
             </div>
             <div className="row">
-                <div className="col">
-                    <p>{data.language}</p>
+                <div className="col-8">
+                    <h3>README</h3>
+                    <RepositoryMarkdownViewer src={data.getReadmeUrl()}/>
                 </div>
-                <div className="col">
+                <div className="col-4">
+                    <h3>Images</h3>
+                    {/*<Carousel className="media-viewer"></Carousel>*/}
+                    {imageUrls.map(url => <img src={url} key="" ></img>)}
 
-                </div>
-                <div className="col">
-                    
+                    <h3>Collaborators</h3>
+
+                    <h3>Tags</h3>
                 </div>
             </div>
         </div>
