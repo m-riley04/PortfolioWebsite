f24eaed633ab9e3c268e66ab7fdf8af5695b5ef0
Riley Meyerkorth <rileymeyerkorth@gmail.com>
2024-01-20 21:40:15 -0600

    add button group

--- src/components/pages/HomePage.tsx
+++ src/components/pages/HomePage.tsx
@@ -1,15 +1,24 @@
 import {motion} from "framer-motion";
+import NavigationGroup from "../NavigationGroup";
 
 function HomePage() {
-    return (
+    const paths = [
+        {title: "Repositories", path: "repositories"},
+        {title: "Projects", path: "projects"},
+        {title: "Tools", path: "tools"},
+    ]
+;    return (
         <motion.div 
-            className="page-container"
+            className="page-container home"
             
             initial={{opacity: 0}}
             animate={{opacity: 1}}
             exit={{opacity: 0}}
         >
-            <h1>Home</h1>
+            <div>
+                <h1>Welcome to my page!</h1>
+                <NavigationGroup buttons={paths}></NavigationGroup>
+            </div>
         </motion.div>
     );
 }
