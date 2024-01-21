1ecf5d19529d8ec109aec8a8e66677cbab271237
Riley Meyerkorth <rileymeyerkorth@gmail.com>
2024-01-21 16:09:48 -0600

    create page sections with and without the navbar

--- src/components/AnimatedRoutes.tsx
+++ src/components/AnimatedRoutes.tsx
@@ -4,23 +4,47 @@ import RepositoryPage from "./pages/RepositoryPage";
 import ToolsPage from "./pages/ToolsPage";
 import NotFoundPage from "./pages/NotFoundPage";
 import { 
+    Outlet,
     Route, 
     Routes, 
     useLocation
 } from "react-router-dom";
 import { AnimatePresence } from "framer-motion";
+import NavigationBar from "./NavigationBar";
+import ComingSoonPage from "./pages/ComingSoonPage";
 
 function AnimatedRoutes() {
     const location = useLocation();
     return (
         <AnimatePresence mode="wait">
             <Routes location={location} key={location.pathname}>
-                <Route index element={<HomePage/>}/>
-                <Route path="repositories" element={<RepositoryPage/>}/>
-                <Route path="tools" element={<ToolsPage/>}/>
-                <Route path="*" element={<NotFoundPage/>}/>
+                <Route element={<WithoutNavbar/>}>
+                    <Route index element={<HomePage/>}/>
+                </Route>
+                <Route element={<WithNavbar/>}>
+                    <Route path="repositories" element={<RepositoryPage/>}/>
+                    <Route path="projects" element={<ComingSoonPage/>}/>
+                    <Route path="tools" element={<ToolsPage/>}/>
+                    <Route path="research" element={<ComingSoonPage/>}/>
+                    <Route path="*" element={<NotFoundPage/>}/>
+                </Route>
             </Routes>
         </AnimatePresence>
+    );
+}
+
+const WithNavbar = () => {
+    return (
+    <>
+        <NavigationBar/>
+        <Outlet/>
+    </>
+    );
+}
+
+const WithoutNavbar = () => {
+    return (
+        <Outlet/>
     );
 }
 
