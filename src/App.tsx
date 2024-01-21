ca84b442ba2562b30dc5de474c28d59ab2e8d66e
Riley Meyerkorth <rileymeyerkorth@gmail.com>
2024-01-21 16:09:29 -0600

    consolidate navigation bar into routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,15 @@
-import NavigationBar from "./components/NavigationBar";
 import AnimatedRoutes from "./components/AnimatedRoutes";
 import Footer from "./components/Footer";
 import {
   BrowserRouter as Router,
 } from "react-router-dom";
+import ScrollToTop from "./components/ScrollToTop";
 
 function App() {
   return (
     <div id="app">
       <Router>
-        <NavigationBar/>
+        <ScrollToTop/>
         <AnimatedRoutes/>
         <Footer/>
       </Router>
