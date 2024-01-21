7f209a7a46bc003205cbf2f925e787ac92360064
Riley Meyerkorth <rileymeyerkorth@gmail.com>
2024-01-20 21:40:05 -0600

    Create NavigationGroup.tsx

---
+++ src/components/NavigationGroup.tsx
@@ -0,0 +1,15 @@
+import { Link } from "react-router-dom"
+
+interface Button {
+    title:string;
+    path:string;
+}
+function NavigationGroup( { buttons } : { buttons:Array<Button> }) {
+    return (
+        <div className="navigation-group">
+            {buttons.map((button, index) => <Link to={button.path} key={index} className="clickable navigation-group-button">{button.title}</Link>)}
+        </div>  
+    );
+}
+
+export default NavigationGroup;
