b476ea79208354643ac24bb47bfda8054aa07628
Riley Meyerkorth <rileymeyerkorth@gmail.com>
2024-01-21 20:44:15 -0600

    add nested routing

--- src/components/AnimatedRoutes.tsx
+++ src/components/AnimatedRoutes.tsx
@@ -7,22 +7,29 @@ import {
     Outlet,
     Route, 
     Routes, 
-    useLocation
+    useLocation,
+    Navigate
 } from "react-router-dom";
 import { AnimatePresence } from "framer-motion";
 import NavigationBar from "./NavigationBar";
 import ComingSoonPage from "./pages/ComingSoonPage";
+import RepositoryGrid from "./repository/RepositoryGrid";
+import Repository from "./repository/Repository";
 
 function AnimatedRoutes() {
     const location = useLocation();
     return (
-        <AnimatePresence mode="wait">
+        <AnimatePresence mode="wait"> 
             <Routes location={location} key={location.pathname}>
                 <Route element={<WithoutNavbar/>}>
-                    <Route index element={<HomePage/>}/>
+                    <Route index path="/" element={<HomePage/>}/>
                 </Route>
                 <Route element={<WithNavbar/>}>
-                    <Route path="repositories" element={<RepositoryPage/>}/>
+                    <Route path="repositories" element={<RepositoryPage/>}>
+                        <Route index element={<Navigate to="grid" replace />}/>
+                        <Route path="grid" element={<RepositoryGrid state={location.state}/>}/>
+                        <Route path="repository" element={<Repository/>}/>
+                    </Route>
                     <Route path="projects" element={<ComingSoonPage/>}/>
                     <Route path="tools" element={<ToolsPage/>}/>
                     <Route path="research" element={<ComingSoonPage/>}/>
