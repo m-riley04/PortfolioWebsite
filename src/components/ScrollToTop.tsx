86a242bb08cd73105773906c77afa5fa4778dcfc
Riley Meyerkorth <rileymeyerkorth@gmail.com>
2024-01-22 13:45:42 -0600

    add document body scrolling

--- src/components/ScrollToTop.tsx
+++ src/components/ScrollToTop.tsx
@@ -4,6 +4,11 @@ import { useLocation } from 'react-router-dom';
 function ScrollToTop() {
     const { pathname } = useLocation();
     useEffect(() => {
+        document.body.scrollTo({
+            top: 0,
+            left: 0,
+            behavior: "smooth"
+        });
         window.scrollTo({
             top: 0,
             left: 0,
