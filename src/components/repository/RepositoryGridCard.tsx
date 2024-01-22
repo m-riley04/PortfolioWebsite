7274f14b3f3a29ce05cd0e295040ec0dbf6c5108
Riley Meyerkorth <rileymeyerkorth@gmail.com>
2024-01-22 11:47:23 -0600

    add animation elements

--- src/components/repository/RepositoryGridCard.tsx
+++ src/components/repository/RepositoryGridCard.tsx
@@ -2,26 +2,45 @@ import RepositoryData from "../../classes/RepositoryData";
 import { useContext } from "react";
 import CurrentRepositoryContext from "../contexts/CurrentRepositoryContext";
 import RepositoriesPageContext from "../contexts/RepositoriesPageContext";
+import { motion } from "framer-motion";
+
+interface CardAnimations {
+    delay: number,
+    duration: number
+}
 
 /**
 * The card that will be displayed within the RepositoryGrid
 * @param {RepositoryData} data The repository data, which contains information about a repository.
 */
-function RepositoryGridCard({data} : {data:RepositoryData}) {
+function RepositoryGridCard({ data, animations } : {data:RepositoryData, animations?:CardAnimations}) {
     // Get contexts of parent page and the currently selected repository
-    const {currentRepository, setCurrentRepository} = useContext(CurrentRepositoryContext);
-    const {page, setPage} = useContext(RepositoriesPageContext);
+    const { setCurrentRepository } = useContext(CurrentRepositoryContext);
+    const { setPage } = useContext(RepositoriesPageContext);
 
     return (
-            <div className="card clickable" onClick={() => {
-                setCurrentRepository(data);
-                setPage("#repository");
-                console.log(`Selected Repository: ${data}`);
-            }}>
+            <motion.div 
+                className="card clickable" 
+
+                transition={{
+                    delay: animations?.delay,
+                    duration: animations?.duration
+                }}
+                initial={{opacity: 0}}
+                animate={{opacity: 1}}
+                
+                
+
+                onClick={() => {
+                    setCurrentRepository(data);
+                    setPage("#repository");
+                    console.log(`Selected Repository: ${data}`);
+                }}
+                >
                 <img src={data.image} hidden={!data.image}></img>
                 <h2>{data.name}</h2>
                 <p>{data.description}</p>
-            </div>
+            </motion.div>
     );
 }
 
