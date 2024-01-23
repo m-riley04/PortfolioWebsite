acb460cdf99dc9eab50c29c7904fd79fefb04392
Riley Meyerkorth <rileymeyerkorth@gmail.com>
2024-01-22 20:44:15 -0600

    add class to carousel items

--- src/components/repository/RepositoryMediaViewer.tsx
+++ src/components/repository/RepositoryMediaViewer.tsx
@@ -4,7 +4,7 @@ function RepositoryMediaViewer( { urls } : { urls?:Array<string>}) {
 
     return (
         <Carousel className="media-viewer">
-            {urls?.map((url, index) => <Carousel.Item key={index}><img src={url} alt={url}></img></Carousel.Item>)}
+            {urls?.map((url, index) => <Carousel.Item key={index} className="media-viewer-item"><img src={url} alt={url}></img></Carousel.Item>)}
         </Carousel>
     );
 }
