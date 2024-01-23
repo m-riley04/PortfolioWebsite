ae079d84ed0097fdff530649526887057274c539
Riley Meyerkorth <rileymeyerkorth@gmail.com>
2024-01-22 20:43:44 -0600

    move fetchSource outside

--- src/components/repository/RepositoryMarkdownViewer.tsx
+++ src/components/repository/RepositoryMarkdownViewer.tsx
@@ -1,6 +1,22 @@
 import { useState, useEffect } from "react";
 import Markdown from "react-markdown";
 
+
+/**
+ * Fetches the markdown text from src url property
+ */
+const fetchSource = (src:string) => {
+    console.log(`Fetching the markdown source at ${src}...`)
+    return fetch( src )
+        .then ( (response) => {
+            if (!response.ok) {
+                throw new Error("ERROR: There was an error fetching the markdown text.")
+            }
+            return response.text();
+        })
+}
+
+
 /**
  * @param {string} src A url to a markdown file 
  */
@@ -9,28 +25,15 @@ function RepositoryMarkdownViewer( { src } : { src: string } ) {
     // Set up states and hooks
     const [text, setText] = useState("");
 
-    /**
-     * Fetches the markdown text from src url property
-     */
-    const fetchSource = () => {
-        console.log(`Fetching the markdown source at ${src}...`)
-        fetch( src )
-            .then ( (response) => {
-                if (!response.ok) {
-                    setText("### There was an issue fetching the repository's README. \nIt might be because 'README.md' does not exist at the repository's root.")
-                    throw new Error("ERROR: There was an error fetching the markdown text.")
-                }
-                return response.text();
-            })
+    useEffect(() => {
+        fetchSource(src)
             .then( (text) => {
                 setText(text);
-            }).catch ( (err) => {
+            })
+            .catch( (err) => {
+                setText("### There was an issue fetching the repository's README. \nIt might be because 'README.md' does not exist at the repository's root.")
                 console.error(err);
             })
-    }
-
-    useEffect(() => {
-        fetchSource();
     })
 
     return (
