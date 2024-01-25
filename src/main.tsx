824b28201e8e2a0a7d8733c9f185d82fc3cc1cf6
Riley Meyerkorth <rileymeyerkorth@gmail.com>
2024-01-24 23:13:07 -0600

    add apollo client

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,13 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import '/src/scss/styles.scss'
+import { ApolloProvider } from '@apollo/client'
+import { client } from './client.ts'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>
   </React.StrictMode>,
 )
