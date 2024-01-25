298ff50a88bc6731ba9e86aebaa37b0d47a05976
Riley Meyerkorth <rileymeyerkorth@gmail.com>
2024-01-24 23:13:47 -0600

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -8,11 +8,11 @@ import ScrollToTop from "./components/ScrollToTop";
 function App() {
   return (
     <div id="app">
-      <Router>
-        <ScrollToTop/>
-        <AnimatedRoutes/>
-        <Footer/>
-      </Router>
+        <Router>
+          <ScrollToTop/>
+          <AnimatedRoutes/>
+          <Footer/>
+        </Router>
     </div>
   );
 }
