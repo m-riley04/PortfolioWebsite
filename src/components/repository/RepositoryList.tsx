006743eff13346928544cb886bfea92d24a492f5
Riley Meyerkorth <rileymeyerkorth@gmail.com>
2024-01-27 22:55:19 -0600

    make "repo" optional

--- src/components/repository/RepositoryList.tsx
+++ src/components/repository/RepositoryList.tsx
@@ -1,10 +1,10 @@
 import RepositoryListItem from './RepositoryListItem';
 import { Repository } from "../../graphql/Query.ts";
 
-function RepositoryList( { repos } : { repos:Array<Repository>}) {
+function RepositoryList( { repos } : { repos?:Array<Repository> | undefined } ){
     return (
         <ul className="list-group list">
-            {repos.map((repo, index) => <RepositoryListItem key={index} repo={repo}/>)}
+            {repos?.map((repo, index) => <RepositoryListItem key={index} repo={repo}/>)}
         </ul>
     );
 }
