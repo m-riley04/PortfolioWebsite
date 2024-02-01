917a20b8074433aafb79da36254e9242aa366171
Riley Meyerkorth <rileymeyerkorth@gmail.com>
2024-02-01 12:46:05 -0600

    add import for banner image

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,5 +1,6 @@
 import {motion} from "framer-motion";
 import NavigationGroup from "../components/NavigationGroup";
+import bannerSrc from "../../assets/home_banner.jpg";
 
 /**
  * The home/root page of the website
@@ -20,7 +21,7 @@ function HomePage() {
             animate={{opacity: 1}}
             exit={{opacity: 0}}
         >
-            <div id="banner">
+            <div id="banner" style={{background: `linear-gradient(0deg, rgba(0, 0, 0, 0.4), rgba(0, 0, 0, 0.4)), url(${bannerSrc})`}}>
                 <h1>Welcome to my page!</h1>
                 <NavigationGroup buttons={paths}></NavigationGroup>
             </div>
