98a39e525e5bc9a692725e5693217a3e6f755567
Riley Meyerkorth <rileymeyerkorth@gmail.com>
2024-02-03 21:07:03 -0600

    Create RepositoryLatestReleaseViewer.tsx

---
+++ src/components/repository/repositoryviewer/RepositoryLatestReleaseViewer.tsx
@@ -0,0 +1,15 @@
+
+/**
+ * 
+ * @returns an element that displays the latest release
+ */
+function RepositoryLatestReleaseViewer() {
+
+    return (
+        <div className="latest-release-viewer">
+            
+        </div>
+    );
+}
+
+export default RepositoryLatestReleaseViewer;
